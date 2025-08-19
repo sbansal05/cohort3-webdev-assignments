415901233f26bf74be80a939956b943449a12543
sbansal05 <siddhi.bansal0504@gmail.com>
2025-08-19 19:17:09 +0530

    Update bookmarks.js

--- week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
+++ week-6/6.2-bookmark-manager/backend/routes/bookmarks.js
@@ -1,13 +1,44 @@
 let bookmarks = []; // in memory space
+let currId = 1;
 
 export async function addBookmark(req,res,next){
-// write here
+    try {
+        const {category, url} = req.body;
+        if(!category || !url) {
+            return res.status(400).json({error: 'Category and Url required'});
+        }
+
+        const newBookmark = {id: currId++ , category, url};
+        bookmarks.push(newBookmark);
+        return res.status(200).json({newBookamrk});
+    } catch(error) {
+        res.status(500).json({message: 'An error occurred while adding bookmark'});
+
+    }
+
+
 }
 
 export async function deleteBookmark(req,res,next){
-// write here
+    try {
+        const {id} = req.params;
+        const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id == id);
+        if(!bookmarkIndex) {
+            res.status(404).json({error: "bookmark not found"});
+        }
+        bookmarks.splice(bookmarkIndex, 1);
+        res.status(200).json({message: "Bookmark deleted"});
+
+    } catch {
+        res.status(500).json({error: "An error occured while deleting the bookmark"});
+    }
+    
+
+
+
 }
 
 export async function getAllBookmarks(req,res,next){
-// write here
+    res.json(bookmarks);
+
 }
